cac854f6dbc23456075f4db0c57924adbe8cf2f4
Hamish Arnold <78823477+h-arnold@users.noreply.github.com>
2025-02-19 10:52:48 +0000

    Moved the global functions related to init and updates to globals.js to tidy up z_main.js

--- src/frontend/UpdateAndInitManager/globals.js
+++ src/frontend/UpdateAndInitManager/globals.js
@@ -1,3 +1,5 @@
+// This file contains the global functions related to initialisation and updates.
+
 /**
  * Global function to handle version updates.
  * @param {Object} versionData Object containing version and file IDs
@@ -7,3 +9,23 @@ function handleVersionUpdate(versionData) {
     const updateController = new UpdateController();
     return updateController.updateAdminSheet(versionData);
 }
+
+/**
+ * Global function to launch the Update Assessment Records Wizard.
+ * Google Apps Script cannot call class methods directly so this function creates an instance
+ * of UpdateManager and then calls the runAssessmentRecordUpdateWizard method.
+ */
+function showUpdateAssessmentRecordWizard() {
+    const updateManager = new UpdateManager();
+    updateManager.runAssessmentRecordUpdateWizard();
+  }
+  
+  /**
+   * Global function called from the wizard when the user clicks "Finish".
+   * This creates a new UpdateManager instance, loads the saved state, and then calls its updateAssessmentRecords method.
+   */
+  function updateAssessmentRecordsFromWizard() {
+    const updateManager = new UpdateManager();
+    updateManager.loadState();
+    updateManager.updateAssessmentRecords();
+  }
