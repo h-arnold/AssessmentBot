b5a6a779a0b19607d81421b59375e9afb76a5241
Hamish Arnold <work@hamisharnold.co.uk>
2025-03-25 21:54:25 +0000

    Updated `updateClassroomSheetWithNewAsssmentRecords` method to remove unnessecary reference to Admin Sheet Id which was causing the update to fail.

--- src/frontend/UpdateAndInitManager/UpdateManager.js
+++ src/frontend/UpdateAndInitManager/UpdateManager.js
@@ -6,21 +6,21 @@ class UpdateManager extends BaseUpdateAndInit {
   constructor() {
     // Call the superclass constructor.
     super();
-    
+
     // Ensure this is being run from an Admin Sheet.
     Utils.validateIsAdminSheet(true);
-    
+
     // Additional initialization specific to UpdateManager.
     this.classroomSheet = new ClassroomSheetManager('Classrooms', this.sheet.getId());
-    
+
     // Set the admin sheet template from version details.
     this.adminSheetTemplateId = this.getLatestAdminSheetTemplateId();
-    
+
     // Objects to store details.
     this.assessmentRecordSheets = {};
     this.adminSheetsDetails = {};
   }
-  
+
   /**
    * Retrieves the admin sheet details for cloning.
    * @returns {Object} An object with the admin sheet details.
@@ -35,18 +35,23 @@ class UpdateManager extends BaseUpdateAndInit {
     this.adminSheetsDetails = adminSheet;
     return adminSheet;
   }
-  
+
   /**
    * Updates the 'Classrooms' sheet in the new Admin Spreadsheet with new Assessment Record file IDs.
    */
   updateClassroomSheetWithNewAssessmentRecords(newAssessmentRecordSheets) {
-    const newAdminSheetName = Object.keys(this.adminSheetsDetails)[0];
-    const newClassroomSheet = new ClassroomSheetManager('Classrooms', this.adminSheetsDetails[newAdminSheetName].newSheetId);
+
+    // Gets the 'Classrooms' Sheet in the Admin Sheet.
+    const newClassroomSheet = new ClassroomSheetManager('Classrooms');
+
+    // Gets the existing sheet IDs (which have now been moved to the 'Archive {todays's date} folder)
     const currentValues = newClassroomSheet.getData();
     let updatedValues = currentValues;
+
+    // Finds the column index of the assessment records.
     let arFileIdColumnIndex = newClassroomSheet.getColumnIndicesFromHeader('AR File ID');
     arFileIdColumnIndex = arFileIdColumnIndex.arfileid;
-  
+
     // Update Classroom Sheet Array with new Sheet Values
     Object.keys(newAssessmentRecordSheets).forEach(className => {
       const sheetDetails = this.assessmentRecordSheets[className];
@@ -59,7 +64,7 @@ class UpdateManager extends BaseUpdateAndInit {
     });
     this.classroomSheet.setData(updatedValues);
   }
-  
+
   /**
    * Moves the old versions of the Assessment Records and Admin Sheet to an Archive folder.
    * @param {Array} assessmentRecordFileIds - An array of file IDs to archive.
@@ -68,16 +73,16 @@ class UpdateManager extends BaseUpdateAndInit {
     const parentFolder = DriveManager.getParentFolderId(this.sheet.getId());
     const date = Utils.getDate();
     const archiveFolder = DriveManager.createFolder(parentFolder, `Archive ${date}`);
-    
+
     // If no array is passed, default to the assessmentRecordSheets details.
     if (!assessmentRecordFileIds) {
       assessmentRecordFileIds = Object.values(this.assessmentRecordSheets).map(item => item.originalSheetId);
     }
-    
+
     // Move all the files to the archive folder
     DriveManager.moveFiles(archiveFolder.newFolderId, assessmentRecordFileIds, ` - ARCHIVED - ${date}`);
   }
-  
+
   /**
    * Updates the admin sheet by creating a new version and archiving the old one.
    * This process includes:
@@ -91,36 +96,36 @@ class UpdateManager extends BaseUpdateAndInit {
     // Update configuration values prior to cloning.
     configurationManager.setUpdateStage(1);
     configurationManager.setIsAdminSheet(true);
-    
+
     // Serialise existing config
     const propsCloner = new PropertiesCloner();
     propsCloner.serialiseProperties();
-    
+
     const adminSheetDetails = this.getAdminSheetDetails();
     const adminSheetName = Object.keys(this.adminSheetsDetails)[0];
     const currentAdminSheetFileId = this.adminSheetsDetails[adminSheetName].originalSheetId;
-    
+
     // Assumes that we want the admin sheet to be in the same folder as the last one.
     const parentFolderId = DriveManager.getParentFolderId(currentAdminSheetFileId);
     this.destinationFolderId = parentFolderId;
-    
+
     // Clone the admin sheet.
     this.cloneSheets(adminSheetDetails, this.destinationFolderId, this.adminSheetTemplateId);
-    
+
     // The newSheetId property should be populated now.
     const newSheetId = this.adminSheetsDetails[adminSheetName].newSheetId;
-    
+
     // Archive the old admin sheet.
     this.archiveOldVersions([currentAdminSheetFileId]);
-    
+
     // Open the new admin sheet in a new window.
     const newSheetUrl = SpreadsheetApp.openById(newSheetId).getUrl();
     this.uiManager.openUrlInNewWindow(newSheetUrl);
-    
+
     // Returns the URL so that it can be fed to the GuiManager.ui.
     return newSheetUrl;
   }
-  
+
   /**
    * Gets the class name and file ID of all assessment records listed in the 'Classrooms' sheet.
    */
@@ -129,7 +134,7 @@ class UpdateManager extends BaseUpdateAndInit {
     const sheetsData = this.classroomSheet.getData();
     // Remove header row.
     sheetsData.shift();
-    
+
     let assessmentRecordSheets = {};
     sheetsData.forEach(row => {
       // Adds an element to the assessmentRecordSheets object if there's a file ID in the row.
@@ -143,7 +148,7 @@ class UpdateManager extends BaseUpdateAndInit {
     this.assessmentRecordSheets = assessmentRecordSheets;
     return assessmentRecordSheets;
   }
-  
+
   /**
    * Updates all assessment records by cloning them into the latest template and archiving old versions.
    * This method performs the following steps:
@@ -158,35 +163,35 @@ class UpdateManager extends BaseUpdateAndInit {
     this.progressTracker = new ProgressTracker();
     const uiManager = new UIManager();
     uiManager.showProgressModal();
-    
+
     let step = 0;
     this.progressTracker.startTracking('Updating all Assessment Records. This may take a while...');
-    
+
     // Gets the assessment record template file Id - should have been set when the admin sheet was updated.
     this.assessmentRecordTemplateId = configurationManager.getAssessmentRecordTemplateId();
-    
+
     this.progressTracker.updateProgress(++step, 'Fetching Assessment Record Details');
     // Get the assessment record details.
     this.getAssessmentRecordDetails();
-    
+
     // Clone the assessment record sheets.
     this.progressTracker.updateProgress(++step, 'Cloning Assessment Record sheets into latest template');
     const newAssessmentRecordSheets = this.cloneSheets(this.assessmentRecordSheets);
-    
+
     // Archive old assessment record sheets.
     this.progressTracker.updateProgress(++step, 'Archiving old Assessment Record sheets');
     const assessmentRecordFileIds = Object.values(this.assessmentRecordSheets).map(item => item.originalSheetId);
     this.archiveOldVersions(assessmentRecordFileIds);
-    
+
     // Update the Classroom Sheet with the new Assessment Record file IDs.
     this.progressTracker.updateProgress(++step, 'Updating Classroom Sheet with new Assessment Record File IDs');
     this.updateClassroomSheetWithNewAssessmentRecords(newAssessmentRecordSheets);
-    
+
     // Marks the task as complete.
     this.progressTracker.complete();
     configurationManager.setUpdateStage(2);
   }
-  
+
   /**
    * Modified runAssessmentRecordUpdateWizard method that launches the HTML-powered wizard.
    * Prepares the necessary parameters, saves state, and then displays the wizard.
@@ -195,29 +200,29 @@ class UpdateManager extends BaseUpdateAndInit {
     // Deserialise Script and User Properties transferred from previous sheet.
     const propertiesCloner = new PropertiesCloner();
     propertiesCloner.deserialiseProperties(true);
-    
+
     // Retrieve the destination folder ID from configuration.
     this.destinationFolderId = configurationManager.getAssessmentRecordDestinationFolder();
-    
+
     // Make a local copy of the Assessment Record Template.
     this.assessmentRecordTemplateId = this.copyAssessmentRecordTemplate();
-    
+
     // Store the fileId of the 'local' copy of the assessment record in the config files.
     configurationManager.setAssessmentRecordTemplateId(this.assessmentRecordTemplateId);
     const assessmentRecordTemplateUrl = this.getAssessmentRecordTemplateUrl(this.assessmentRecordTemplateId);
-    
+
     // Retrieve the Script ID.
     const sa = new ScriptAppManager();
     const adminScriptId = sa.getScriptId();
-    
+
     // Save the current state so that it can be retrieved later in updateAssessmentRecords.
     this.saveState();
-    
+
     // Create the HTML template for the wizard and pass in the template URL and script ID.
     const template = HtmlService.createTemplateFromFile('UpdateAndInitManager/UpdateWizard');
     template.assessmentRecordTemplateUrl = assessmentRecordTemplateUrl;
     template.adminScriptId = adminScriptId;
-    
+
     const htmlOutput = template.evaluate().setWidth(600).setHeight(400);
     SpreadsheetApp.getUi().showModalDialog(htmlOutput, 'Update Assessment Records Wizard');
   }
