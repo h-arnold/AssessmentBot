b52133c07fe35a3d89185166704b0423ae3d0856
Hamish Arnold <78823477+h-arnold@users.noreply.github.com>
2025-12-04 12:01:44 +0000

    feat: update version number to 0.7.5 in BaseUpdateAndInit class

--- src/AdminSheet/UpdateAndInitManager/BaseUpdateAndInitManager.js
+++ src/AdminSheet/UpdateAndInitManager/BaseUpdateAndInitManager.js
@@ -17,7 +17,7 @@ class BaseUpdateAndInit {
     this.sheet = SpreadsheetApp.getActiveSpreadsheet();
     this.uiManager = UIManager.getInstance();
     this.destinationFolderId = ''; // to be set later from configuration or during the process
-    this.versionNo = '0.7.4';
+    this.versionNo = '0.7.5';
     // This needs to before getting the new template Ids otherwise they'll stay with whatever value was set before.
     this.versionDetails = this.fetchVersionDetails();
     // Retrieve stored Assessment Record Template ID (may be empty string if unset)
