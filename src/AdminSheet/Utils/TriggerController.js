648e0a777ae1ce12fa22dc06a6fd4f89e112ddd6
Hamish Arnold <78823477+h-arnold@users.noreply.github.com>
2026-01-05 15:18:20 +0000

    feat: update OAuth scopes to readonly for Classroom courses and coursework

--- src/AdminSheet/Utils/TriggerController.js
+++ src/AdminSheet/Utils/TriggerController.js
@@ -120,11 +120,11 @@ class TriggerController {
 // Defined as a static property to avoid recreating the array per instance.
 TriggerController.REQUIRED_SCOPES = [
   'https://www.googleapis.com/auth/presentations',
-  'https://www.googleapis.com/auth/classroom.courses',
-  'https://www.googleapis.com/auth/classroom.rosters',
+  'https://www.googleapis.com/auth/classroom.courses.readonly',
+  'https://www.googleapis.com/auth/classroom.rosters.readonly',
   'https://www.googleapis.com/auth/classroom.profile.emails',
   'https://www.googleapis.com/auth/classroom.profile.photos',
-  'https://www.googleapis.com/auth/classroom.coursework.students',
+  'https://www.googleapis.com/auth/classroom.coursework.students.readonly',
   'https://www.googleapis.com/auth/spreadsheets',
   'https://www.googleapis.com/auth/drive',
   'https://www.googleapis.com/auth/script.storage',
